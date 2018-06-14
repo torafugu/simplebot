27537962a9b05947bdc8beeffb7c4b33e8e69d76
Yoshihito Kaneda <Yoshihito.Kaneda@jda.com>
2018-06-14 15:12:19 +0900

    kuromoji.jsを使った簡単な形態素解析を追加。

--- client/main.js
+++ client/main.js
@@ -1,15 +1,11 @@
 import { Template } from 'meteor/templating';
 import { ChatLogs } from '../api/chatlogs.js';
-
 import './main.html';
 
 Template.body.helpers({
   chatLogs: function () {
     return ChatLogs.find({});  
   }
-  // 'commenter': function(){
-  //   return PlayersList.find({}, { sort: {score: -1, name: 1} });
-  // },
 });
 
 Template.addComment.events({
@@ -17,20 +13,8 @@ Template.addComment.events({
     event.preventDefault();
     var commentVar = event.target.comment.value;
 
-    // Comment
-    ChatLogs.insert({
-      commenter: 0,
-      text: commentVar,
-      createdAt: new Date()
-    });
-
-    // Reply
-    ChatLogs.insert({
-      commenter: 1,
-      text: commentVar += 'ってどうなの？',
-      createdAt: new Date()
-    });
-
+    Meteor.call('converse', commentVar, function(error, result) {});
+    
     event.target.comment.value = "";
   }
 });
--- server/main.js
+++ server/main.js
@@ -1,5 +1,72 @@
 import { Meteor } from 'meteor/meteor';
+import { ChatLogs } from '../api/chatlogs.js';
+
+var kuromoji = require('kuromoji');
+var builder = kuromoji.builder({
+  dicPath: 'C:/Users/1019547/simplebot/node_modules/kuromoji/dict'
+});
+
+// const bound = Meteor.bindEnvironment((callback) => {callback()});
 
 Meteor.startup(() => {
-  // code to run on server at startup
 });
+
+Meteor.methods({
+  // helloメソッドの定義
+  converse: function(rcvdMsg) {
+ 
+    // Call
+    ChatLogs.insert({
+      commenter: 0,
+      text: rcvdMsg,
+      createdAt: new Date()
+    });
+
+    console.log(morph(rcvdMsg));
+
+    // Reply
+    ChatLogs.insert({
+      commenter: 1,
+      text: morph(rcvdMsg),
+      createdAt: new Date()
+    });           
+  }
+});
+
+function morph(rcvdMsg) {builder.build(function(err, tokenizer) {
+  if(err) {
+    console.log(err);
+    //throw new Meteor.Error(err);
+  } else {
+    
+    var analyzedResult = tokenizer.tokenize(rcvdMsg);
+    var nounFlg = false;
+    var tgtNoun = '';
+    var verbFlg = false;
+    var adjectiveFlg = false;
+    var reply;
+
+    analyzedResult.forEach(function(word) {
+      switch (word.pos) {
+        case '名詞':
+            nounFlg = true;
+            tgtNoun = word.surface_form;
+            break;
+        case '動詞':
+            verbFlg = true;
+            break;
+        case '形容詞':
+            adjectiveFlg = true;
+            break;
+      }
+    });
+
+    if (nounFlg) {
+      reply = tgtNoun + 'ってなに？';
+    } else {
+      reply = 'どゆこと？';
+    }
+    console.log('1.' + reply);
+  }
+})};
+
